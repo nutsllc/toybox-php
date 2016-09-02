88b1fd9c1c7dfb217a2d112f1d0a3cf6404342a0
@ontheroad_jp <dev@ontheroad.jp>
2016-09-02 16:42:48 +0900

    [Fix] GID and UID setting

--- 5.6/apache/docker-entrypoint.sh
+++ 5.6/apache/docker-entrypoint.sh
@@ -4,27 +4,29 @@ set -e
 user="www-data"
 group="www-data"
 
-if [ -n "${TOYBOX_GID+x}" ]; then
+if [ -n "${TOYBOX_GID+x}" ] && ! cat /etc/group | grep ":${TOYBOX_GID}:" > /dev/null 2>&1; then
     groupmod -g ${TOYBOX_GID} ${group}
+    echo "GID of ${group} has been changed."
 fi
 
-if [ -n "${TOYBOX_UID+x}" ]; then
+if [ -n "${TOYBOX_UID+x}" ] && ! cat /etc/passwd | grep ":${TOYBOX_UID}:" > /dev/null 2>&1; then
     usermod -u ${TOYBOX_UID} ${user}
+    echo "UID of ${user} has been changed."
 fi
 
-docroot="/var/www/html"
+docroot="/usr/local/apache2/htdocs"
 mkdir -p ${docroot}
-tar xvzf /usr/src/apache2-default-doc.tar.gz -C ${docroot}
+tar xzf /usr/src/apache2-default-doc.tar.gz -C ${docroot}
 chown -R ${user}:${group} ${docroot}
 
 apache2_confdir="/etc/apache2"
 mkdir -p ${apache2_confdir}
-tar xvzf /usr/src/apache2-conf.tar.gz -C ${apache2_confdir}
+tar xzf /usr/src/apache2-conf.tar.gz -C ${apache2_confdir}
 chown -R ${user}:${group} ${apache2_confdir}
 
 php_confdir="/usr/local/etc/php"
 mkdir -p ${php_confdir}
-tar xvzf /usr/src/php-conf.tar.gz -C ${php_confdir}
+tar xzf /usr/src/php-conf.tar.gz -C ${php_confdir}
 chown -R ${user}:${group} ${php_confdir}
 
 exec "$@"
