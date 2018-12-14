727bc044535e6eeabdfef52a019860756016a9ec
@ontheroad_jp <dev@ontheroad.jp>
2018-12-15 02:40:47 +0900

    [New] PHP 7.1, 7,2

--- bin/init.sh
+++ bin/init.sh
@@ -8,6 +8,10 @@ dirs=(
     "${dist}/5.6/fpm"
     "${dist}/7.0/apache"
     "${dist}/7.0/fpm"
+    "${dist}/7.1/apache"
+    "${dist}/7.1/fpm"
+    "${dist}/7.2/apache"
+    "${dist}/7.2/fpm"
 )
 for d in ${dirs[@]}; do
     src=${self}/../seed/ubuntu
@@ -17,10 +21,27 @@ for d in ${dirs[@]}; do
         php_ver=5.6.23
         apcu_ver=4.0.11
         redis_ver=2.2.8
+        xdebug_ver=2.4.1
     elif [ ${php_ver} = "7.0" ]; then
         php_ver=7.0.8
         apcu_ver=5.1.6
         redis_ver=3.0.0
+        xdebug_ver=2.4.1
+    elif [ ${php_ver} = "7.1" ]; then
+        php_ver=7.1.25
+        apcu_ver=5.1.15
+        redis_ver=3.1.6
+        xdebug_ver=2.6.0
+    elif [ ${php_ver} = "7.2" ]; then
+        php_ver=7.2.13
+        apcu_ver=5.1.15
+        redis_ver=4.2.0
+        xdebug_ver=2.6.0
+    elif [ ${php_ver} = "7.3" ]; then
+        php_ver=7.3
+        apcu_ver=5.1.15
+        redis_ver=4.2.0
+        xdebug_ver=2.6.0
     fi
     php_type=$(echo ${d} | awk -F "/" '{print $NF}')
     dockerfile=${src}/${php_type}/Dockerfile-seed
@@ -35,6 +56,7 @@ for d in ${dirs[@]}; do
     sed -i -e "s/{{ENV_PHP_VERSION}}/${php_ver}/" ${d}/Dockerfile
     sed -i -e "s/{{APCU_VERSION}}/${apcu_ver}/" ${d}/Dockerfile
     sed -i -e "s/{{REDIS_VERSION}}/${redis_ver}/" ${d}/Dockerfile
+    sed -i -e "s/{{XDEBUG_VERSION}}/${xdebug_ver}/" ${d}/Dockerfile
     echo "done."
 done
 
